376b3ecc003e7f574b43bb0982ea909ca7caad35
arjunsagarkpc <mail.arjunsagar@gmail.com>
2023-07-29 18:08:13 -0400

    Add project files.

---
+++ JenkinsPOC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using JenkinsPOC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JenkinsPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return Content("Hello, Jenkins!");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
